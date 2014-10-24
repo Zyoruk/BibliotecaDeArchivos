68bc34e88cf903444a2f44c52274faaf4f4f1e85
jeukel <jeukel@gmail.com>
2014-10-24 13:00:29 -0600

    Creating methods for read/write.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include "konstants.h"
-#include<limits>
+#include <limits>
+#include <vector>
 //#define STR(x) #x << '=' << x
 
 using namespace std;
@@ -16,7 +17,7 @@ void setup()
     K = new konstants();
 }
 
-void createNewFile(string newFileName){
+fstream& createNewFile(string newFileName){
     string newFileDir = K->dirFile;
     newFileDir.append(newFileName);
     cout << newFileDir << endl;
@@ -25,104 +26,103 @@ void createNewFile(string newFileName){
         cout << "****Database succesfully created***" << endl;
     else
         cout << "****Database could not be created***" << endl;
-    database.close();    
+    return &database;
 }
 
- void createTable(){
+ void createTable(string &ColumnNames, int &ColumnSizes){
      cout << "****Name for new table***" << endl;
      cin >> newFileName;
-     createNewFile(newFileName);
+     ofstream* database = createNewFile(newFileName);
+     for_each
+
  }
+
+void updateField(){
+    ofstream file (K->dirFile.c_str() , ios::app);
+    if(data.size() <= 64){
+
+    }
+    file.close();
+}
+
+void readField(){
+    ifstream file (K->dirFile.c_str());
+    char character;
+
+    int row = (6 - 1) * 128;
+    //show name
+    file.seekg(row);
+    while (file.tellg() < (64 * row)){
+        character = file.get();
+        if( character == '*'){
+            cout << ' ' << endl;
+            break;
+        }
+        cout << character;
+    }
+
+    cout << "------------------------------------------" << endl;
+
+    //show lastname
+    file.seekg(row + 64, ios::beg);
+    while (file.tellg() < (128 * row)){
+        character = file.get();
+        if( character == '*'){
+            cout << ' ' << endl;
+            break;
+        }
+        cout << character;
+    }
+
+    /*
+    //show adress
+    file.seekg(row + 128, ios::beg);
+    while (file.tellg() < 256 * row){
+        character = file.get();
+        if( character == '*'){
+            cout << ' ' << endl;
+            break;
+        }
+        cout << number;
+    }
+
+    //show country
+    file.seekg(row + 256, ios::beg);
+    while (file.tellg() < 320 * row){
+        character = file.get();
+        if( character == '*'){
+            cout << ' ' << endl;
+            break;
+        }
+        cout << number;
+    }
+
+    //show line
+    string t;
+    while(getline (file,t)){
+        if(t != "\n"){
+            cout << t << endl;
+        }
+    }*/
+}
 
 int main()
 {
     setup();
+    cout << "****Field operations***" << endl;
+    cout << "****1. Edit ****" << endl;
+    cout << "****2. Read ****" << endl;
 
-
-    cout << "****Name for new table***" << endl;
-    cout << "****1. Edit****" << endl;
-    cout << "****2. Read****" << endl;
     cin >> option;
     switch (option) {
         case 1:
         {
-            cout << "****************************" << endl;
-            cout << "****Opening datafile for editing****" << endl;
-            cout << "****************************" << endl;
-            cout << " " << endl;
-
-            ofstream file (K->dirFile.c_str() , ios::app);
-            if(data.size() <= 64){
-
-            }
-
-            file.close();
+            updateField();
             break;
         }
         case 2:
         {
-            cout << "****************************" << endl;
-            cout << "****Opening datafile for consulting****" << endl;
-            cout << "****************************" << endl;
-            cout << " " << endl;
-            ifstream file (K->dirFile.c_str());
-            char character;
-
-            int row = (6 - 1) * 128;
-            //show name
-            file.seekg(row);
-            while (file.tellg() < (64 * row)){
-                character = file.get();
-                if( character == '*'){
-                    cout << ' ' << endl;
-                    break;
-                }
-                cout << character;
-            }
-
-            cout << "------------------------------------------" << endl;
-
-            //show lastname
-            file.seekg(row + 64, ios::beg);
-            while (file.tellg() < (128 * row)){
-                character = file.get();
-                if( character == '*'){
-                    cout << ' ' << endl;
-                    break;
-                }
-                cout << character;
-            }
-
-            /*
-            //show adress
-            file.seekg(row + 128, ios::beg);
-            while (file.tellg() < 256 * row){
-                character = file.get();
-                if( character == '*'){
-                    cout << ' ' << endl;
-                    break;
-                }
-                cout << number;
-            }
-
-            //show country
-            file.seekg(row + 256, ios::beg);
-            while (file.tellg() < 320 * row){
-                character = file.get();
-                if( character == '*'){
-                    cout << ' ' << endl;
-                    break;
-                }
-                cout << number;
-            }
-
-            //show line
-            string t;
-            while(getline (file,t)){
-                if(t != "\n"){
-                    cout << t << endl;
-                }
-            }*/
+            readField();
             break;
         }
         default:
