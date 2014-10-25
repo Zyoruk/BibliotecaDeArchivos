4896298861a5c885093b72bf826efb571bd6ef44
Luis Ángel Simón <luisangel1993julio@gmail.com>
2014-10-25 13:15:59 -0600

    Methods to read columns.

--- konstants.h
+++ konstants.h
@@ -9,6 +9,10 @@ class konstants
 public:
     konstants();
     const string dirFile = "../FSQL/";
+    const int ONE_BYTE = 1;
+    const int THREE_BYTES = 3;
+    const int HEADER_FILE_SIZE = 4;
+    const string EMPTY_STRING = "";
 };
 
 #endif // KONSTANTS_H
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "konstants.h"
 #include <limits>
 #include <vector>
+#include <cstdlib>
 //#define STR(x) #x << '=' << x
 
 using namespace std;
@@ -11,7 +12,6 @@ konstants* K;
 int option;
 string data;
 string newFileName;
-
 void setup()
 {
     K = new konstants();
@@ -33,8 +33,6 @@ fstream& createNewFile(string newFileName){
      cout << "****Name for new table***" << endl;
      cin >> newFileName;
      ofstream* database = createNewFile(newFileName);
-     for_each
-
  }
 
 void updateField(){
@@ -45,92 +43,99 @@ void updateField(){
     file.close();
 }
 
-void readField(){
-    ifstream file (K->dirFile.c_str());
-    char character;
+int& stringToInt(string* pStr){
+    int i = atoi(pStr);
+    return &i;
+}
 
-    int row = (6 - 1) * 128;
-    //show name
-    file.seekg(row);
-    while (file.tellg() < (64 * row)){
-        character = file.get();
-        if( character == '*'){
-            cout << ' ' << endl;
-            break;
-        }
-        cout << character;
+string& charCallocToString(char* pCharCalloc){
+    string stringToReturn = "";
+    for (int i = NULL; i <= K->THREE_BYTES;i++){
+        stringToReturn.append(*(pCharCalloc + i));
     }
+    return &stringToReturn;
+}
 
-    cout << "------------------------------------------" << endl;
+int& getRegisterSize(ifstream* pFile){
+    pFile->seekg(K->FOUR_BYTES);
+    char* charString = calloc(3 ,1);
+    pFile->read(charString, K->THREE_BYTES);
+    string* regSizeString = charCallocToString(charString);
+    int* regSize  = stringToInt(regSizeString);
+    return regSize;
+}
 
-    //show lastname
-    file.seekg(row + 64, ios::beg);
-    while (file.tellg() < (128 * row)){
-        character = file.get();
-        if( character == '*'){
-            cout << ' ' << endl;
-            break;
-        }
-        cout << character;
+int& columnSize(ifstream* pFile , int pColumnInt){
+    pFile->seekg((K->FOUR_BYTES + K->THREE_BYTES) + ((pColumnInt - K->ONE_BYTE) * K->THREE_BYTES)); //Move the seek to the beginning of the column.
+    char* charString = calloc(K->THREE_BYTES, K->ONE_BYTE);
+    pFile->read(charString , K->THREE_BYTES);
+    string cSize = K->EMPTY_STRING;
+    // build the string;
+    for (int i = 0; i <= 3;i++){
+        cSize.append(*(charString + i));
     }
+    int* cSizeInt = stringToInt(&cSize);
+    return cSizeInt;
+}
 
-    /*
-    //show adress
-    file.seekg(row + 128, ios::beg);
-    while (file.tellg() < 256 * row){
-        character = file.get();
-        if( character == '*'){
-            cout << ' ' << endl;
-            break;
-        }
-        cout << number;
+int& sizeUntilColumn(int pColumn){
+    int sizeToReturn = NULL;
+    for (int i = NULL; i < pColumn; i++){
+        sizeToReturn = (sizeToReturn)+ *columnSize(i);
     }
+    return &sizeToReturn;
+}
 
-    //show country
-    file.seekg(row + 256, ios::beg);
-    while (file.tellg() < 320 * row){
-        character = file.get();
-        if( character == '*'){
-            cout << ' ' << endl;
-            break;
-        }
-        cout << number;
-    }
+string& readField(string* pFile , int pRow , int Column){
+    //Relative route + the name of the file
+    string fileToRead = K->dirFile;
+    fileToRead.append(pFile);
+    ifstream file (fileToRead.c_str());
 
-    //show line
-    string t;
-    while(getline (file,t)){
-        if(t != "\n"){
-            cout << t << endl;
-        }
-    }*/
+
+    //Move seek to the row
+    file.seekg((*getRegisterSize())*pRow);
+
+    //move seek to the beginning of the column
+    int sizeToColumn = *sizeUntilColumn(Column);
+    file.seekg(sizeToColumn);
+
+    //Read the info
+    int cSize = *columnSize(Column);
+
+    char* infoInsideColumn = calloc(cSize, sizeof(char));
+    file.read(infoInsideColumn , cSize);
+
+    //build the stringto return
+    string* stringToReturn = charCallocToString(infoInsideColumn);
+    return stringToReturn;
 }
 
 int main()
 {
-    setup();
-    cout << "****Field operations***" << endl;
-    cout << "****1. Edit ****" << endl;
-    cout << "****2. Read ****" << endl;
+//    setup();
+//    cout << "****Field operations***" << endl;
+//    cout << "****1. Edit ****" << endl;
+//    cout << "****2. Read ****" << endl;
 
-    cin >> option;
-    switch (option) {
-        case 1:
-        {
-            updateField();
-            break;
-        }
-        case 2:
-        {
-            readField();
-            break;
-        }
-        default:
-        {
-            cout << "The cat said miau...\n";
-            break;
-}
-    }
+//    cin >> option;
+//    switch (option) {
+//        case 1:
+//        {
+//            updateField();
+//            break;
+//        }
+//        case 2:
+//        {
+//            readField();
+//            break;
+//        }
+//        default:
+//        {
+//            cout << "The cat said miau...\n";
+//            break;
+//}
+//    }
 
     return 0;
 }
