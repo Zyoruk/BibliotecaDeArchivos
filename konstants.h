04c38d2a881cbca856393caeba30d050ba2e177b
Luis Ángel Simón <luisangel1993julio@gmail.com>
2014-10-25 19:38:32 -0600

    Fixing errors...

--- konstants.h
+++ konstants.h
@@ -8,13 +8,12 @@ class konstants
 {
 public:
     konstants();
-    const string dirFile = "../FSQL/";
     const int ONE_BYTE = 1;
     const string EMPTY_STRING = "";
     const string DIRFILE = "../FSQL/";
     const int DEFAULT_COLUMN_SIZE = 3;
     const int DEFAULT_REGISTER_SIZE = 4;
-    const int METADATA_SIZE_ADDRESS = 3;
+    const int REGISTER_SIZE_ADDRESS = 3;
     const int MAX_REGISTER_SIZE = 999;
     //const int MAX_METADATA_SIZE =
 };
