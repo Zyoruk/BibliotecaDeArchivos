e092c1370fa1d9ab0afe2dc9db0c5d8348922108
Luis Ángel Simón <luisangel1993julio@gmail.com>
2015-02-26 11:22:24 -0600

    ya se puede crear un archivo .bin

--- biblioteca.c
+++ biblioteca.c
@@ -1,5 +1,17 @@
 #include "biblioteca.h"
+#define FILE_ERROR -1
+#define FILE_CREATED 0
 
-biblioteca::biblioteca()
-{
+int crear_bib(char *pathname ,char *mode){
+
+    FILE *newBib;
+    newBib = fopen(pathname , mode);
+
+    if (newBib == NULL){
+        printf ( " Failed.");
+        return FILE_ERROR;
+    }else{
+        printf ( "Succesful.");
+        return  FILE_CREATED;
+    }
 }
--- biblioteca.h
+++ biblioteca.h
@@ -1,10 +1,13 @@
 #ifndef BIBLIOTECA_H
 #define BIBLIOTECA_H
+#include <stdio.h>
 
-class biblioteca
-{
-public:
-    biblioteca();
-};
+typedef struct {
+    char* F_Path;
+}biblioteca;
 
+biblioteca bib ;
+extern int crear_bib(char *pathname , char *mode);
+extern int abrir_bib();
+extern int cerrar_bib();
 #endif // BIBLIOTECA_H
