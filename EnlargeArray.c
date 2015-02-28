ec85522d7fe495c537a4e658b5d9723aa2a677fa
jeukel <jeukel@gmail.com>
2015-02-28 17:32:20 -0600

    != mains for testing.

--- EnlargeArray.c
+++ EnlargeArray.c
@@ -1,10 +1,9 @@
 #include "EnlargeArray.h"
 
 void elargeArray(void *arraytoElarge){
-    int arraySais = sizeof(&arraytoElarge);
+    int arraySais = sizeof(arraytoElarge[0]);
     int newSais = arraySais + (arraySais * (0.5));
     void *newArray = malloc(newSais);
     arraytoElarge = newArray;
-    return arraytoElarge;
 }
 
