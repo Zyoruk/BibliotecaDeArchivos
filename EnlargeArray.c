721c9aa44ec9650e6bcc9a99cc8a33076c023768
jeukel <jeukel@gmail.com>
2015-03-01 20:51:29 -0600

    Elarge changed.

--- EnlargeArray.c
+++ EnlargeArray.c
@@ -2,14 +2,42 @@
 
 void* elargeIndex(void *arraytoElarge, int var){
     void *newArray = malloc(var * sizeof(struct indexElem));
-    *newArray = *arraytoElarge;
+    for(int i = 0 ; i < (var * sizeof(struct indexElem) ; i++){
+        *newArray[i] = *arraytoElarge[i];
+    }
+    //*newArray = *arraytoElarge;
     free(arraytoElarge);
     return newArray;
 }
 
 void* elargeArray(void *arraytoElarge, int var){
     void *newArray = malloc(var);
-    *newArray = *arraytoElarge;
+    for(int i = 0 ; i < (var * sizeof(struct indexElem) ; i++){
+        *newArray[i] = *arraytoElarge[i];
+    }
+    //*newArray = *arraytoElarge;
     free(arraytoElarge);
     return newArray;
+}
+
+void updateIndex(void *arraytoElarge){
+    var = var++;
+    arraytoElarge = elargeIndex(arraytoElarge, var);
+}
+
+void* reallocateMem(){
+    void* indexList;
+    if(var == 0){
+        indexList = (struct indexElem*) malloc (sizeof(struct indexElem));
+        //printf("pointer address = %p\n", indexList);
+        indexList = updateArray(indexList);
+        //printf("pointer address = %p\n", indexList);
+    }else if (var > 0){
+        indexList = (struct indexElem*) malloc (var * sizeof(struct indexElem));
+        //printf("pointer address = %p\n", indexList);
+        indexList = updateArray(indexList);
+        //printf("pointer address = %p\n", indexList);
+    }else{
+        return -1;
+    }
 }
