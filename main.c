b4847654a90aaefa11f743b2898cdbe03c8ea4f8
Zyoruk <luisangel1993julio@gmail.com>
2015-03-02 15:20:54 -0600

    Almost done

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ void test1 (){
 
     //    cerrar_bib(FH);
         FILE* file = fopen("Test3.txt" , "w+");
-        struct componente_struct ptr;
+        componente_struct ptr;
         ptr.comp_nom = "Hola";
         ptr.rango[0] = 1 ;
         ptr.rango[1] = 2;
@@ -19,7 +19,30 @@ void test1 (){
 
 int main(void)
 {
-    printf("algo");
+    FILE* bib ;
+    bib = fopen("Test14.bin" , "wb+");
+//    char ptr[] = "Luis";
+    int ptr [5]= {3, 4,5 ,6 ,7} ;
+    fwrite(&ptr , sizeof(ptr[0]) , 5, bib);
+    fclose(bib);
+    fopen("Test14.bin" , "rb");
+    int* ptr2 = malloc ( 100);
+    fread(ptr2 , sizeof(ptr2[0]) , 5, bib);
+    fclose(bib);
+
+    printf ( "%d\n" , ptr2[1]);
+
+    fopen("Test14.bin" , "ab");
+    int ptr3 [5] = {12, 13, 14 ,15 ,16};
+    fwrite(&ptr3 , sizeof(ptr3[0]) , 5, bib);
+    fclose(bib);
+    fopen("Test14.bin" , "rb");
+    int* ptr4 = malloc ( 100);
+    fseek(bib , sizeof(ptr3) , SEEK_SET);
+    fread(ptr4 , sizeof(ptr3[0]) , 5, bib);
+    fclose(bib);
+
+    printf ( "%d\n" , ptr4[1]);
     return 0;
 }
 
